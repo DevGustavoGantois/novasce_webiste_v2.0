28917e9b19d25050b4ff98345317cdeae58ffb32
Gustavo Gantois <gugagantois@gmail.com>
2024-12-02 19:42:45 -0300

    hotfix:Retired Button component in s-about for shadcnui

--- src/components/global/navbar-mobile.tsx
+++ src/components/global/navbar-mobile.tsx
@@ -1,9 +1,13 @@
+import { Sheet, SheetTrigger } from "../ui/sheet";
+
 export function NavbarMobile() {
     return (
         <nav className="mb-20 border border-b border-white/20">
-            <div className="absolute top-3 right-3">
-                
-            </div>
+            <Sheet>
+                <SheetTrigger className="absolute top-3 right-3">
+
+                </SheetTrigger>
+            </Sheet>
         </nav>
     )
 }
