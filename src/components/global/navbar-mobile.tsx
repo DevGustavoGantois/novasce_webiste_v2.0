34e00c6b0f8c23581a7ef45bb972f14651ac64f0
Gustavo Gantois <gugagantois@gmail.com>
2024-12-02 19:37:56 -0300

    feat:Adding sheet component for shadcnui

--- src/components/global/navbar-mobile.tsx
+++ src/components/global/navbar-mobile.tsx
@@ -1,7 +1,9 @@
 export function NavbarMobile() {
     return (
-        <nav>
-            
+        <nav className="mb-20 border border-b border-white/20">
+            <div className="absolute top-3 right-3">
+                
+            </div>
         </nav>
     )
 }
